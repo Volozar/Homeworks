52a3c2a9a6653f644da446f3ad4d7224fe28ad84
Volozar <svs.elusive@gmail.com>
2019-07-18 00:08:02 +0300

    js02

--- JS/js/main.js
+++ JS/js/main.js
@@ -1,31 +1,34 @@
 //=====ЗАДАЧА НА ПАЛИНДРОМ=======
-function isPalindrom() {
-    text.split('').reverse().join('') == text ?
-        document.write("Это палиндром") :
-        document.write("Это не палиндром");
+function isPalindrom(a) {
+        a.split('').reverse().join('') == a ?
+        document.writeln("Это палиндром") :
+        document.writeln("Это не палиндром");
+    
 }
 
-// var text = prompt("Напишите слово");
-// isPalindrom(text);
+var text = prompt("Напишите слово");
+
+/* let text = document.getElementById("polindrom").value;
+console.log(typeof(text));
+console.log(text);
+document.writeln(text); */
+isPalindrom(text);
 
 
 //=====ЗАДАЧА НА АРХИВ========
 
 arrTask2 = ["a", "a", "a", 'b', "b", "c", "c", "c", "a", "a", "d"];
 let arrArchive = [];
-let b = 1;
-let a = 0;
+let count = 1;
+let indexArchive = 0;
 
 for(let i = 0; i < arrTask2.length; i++) {
     if(arrTask2[i] === arrTask2[i + 1]){
-        b++;        
+        count++;        
     } else {
-        arrArchive[a] = [arrTask2[i], b];
-        a++;
-        b = 1;
+        arrArchive[indexArchive] = [arrTask2[i], count];
+        indexArchive++;
+        count = 1;
     }
-    
-        
 }
-
 console.log(arrArchive);
